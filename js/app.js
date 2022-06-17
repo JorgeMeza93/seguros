7d6594a13e93cde36d03e87149c910a97318d778
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-17 01:38:11 -0500

    Carrousel completado

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-const contenedorCarrousel = document.querySelector(".carrousel__data");
-const botonIzq = document.querySelector(".primera");
-const botonDer = document.querySelector(".segunda");
-const slides = document.querySelector(".carrousel__slides");
-const botones = document.querySelectorAll(".carrousel__flecha");
+const botones = document.querySelectorAll(".carrousel__boton");
 
-botones.forEach((boton)=> {
+botones.forEach( boton => {
     boton.addEventListener("click", ()=>{
-        const offset = boton.dataset.carouselButton === "next" ? 1 : -1;
+        const offset = boton.dataset.valor === "next" ? 1 : -1;
         console.log(offset);
+        const slides = boton.closest(".carrousel__contendido").querySelector(".carrousel__slides");    //devuelve el ascendiente más cercano al elemento actual en este caso devuelve .contenedor y despues ul
+        console.log(slides);
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        // PARA CICLAR EL slide
+        if(newIndex < 0){
+            newIndex = slides.children.length - 1;
+        }
+        if(newIndex >= slides.children.length){
+            newIndex = 0;
+        }
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active
     });
-   
 });
-
-
-
-
